8e486daf5642e0ec030ef61f9fac262786d27787
Ernst <ernst_luring@hotmail.com>
2016-05-31 14:47:17 +0200

    Update CardsManager.cs

--- Scripts/Framework/CardSystem/Cards/CardsManager.cs
+++ Scripts/Framework/CardSystem/Cards/CardsManager.cs
@@ -19,13 +19,6 @@ public class CardsManager : MonoBehaviour {
 	}
 
 	private void InitializeCards () {
-		// Set the attack & abilities of all card classes in the cardlibrary
-//		foreach (Card card in cardLibrary.cards) {
-//			string attackName = card.name.RemoveWhitespace();
-//			card.SetAttack((BaseAttack)Activator.CreateInstance(Type.GetType(attackName, true)));
-//			string abilityName = card.abilitiesArray[0].RemoveWhitespace();
-//			card.SetAbility((IAbility)Activator.CreateInstance(Type.GetType(abilityName, true)));
-//		}
 		foreach (Card genericCard in cardLibrary.genericCards) {
 			string attackName = genericCard.name.RemoveWhitespace();
 			genericCard.SetAttack((BaseAttack)Activator.CreateInstance(Type.GetType(attackName, true)));

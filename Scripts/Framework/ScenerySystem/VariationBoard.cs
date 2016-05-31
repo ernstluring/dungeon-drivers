707c5f65ad578fc27db598bdc91b27f6481a87c4
Ernst <ernst_luring@hotmail.com>
2016-05-31 14:58:05 +0200

    Update VariationBoard.cs

--- Scripts/Framework/ScenerySystem/VariationBoard.cs
+++ Scripts/Framework/ScenerySystem/VariationBoard.cs
@@ -8,11 +8,6 @@ public class VariationBoard : Board {
 	}
 	
 	public override void Move () {
-//		base.startPosition = myTransform.position;
-//		base.endPosition = base.startPosition + Vector3.back;
-
 		myTransform.Translate(Vector3.back * Time.deltaTime * base.speed, Space.World);
-
-//		transform.position = Vector3.SmoothDamp(base.startPosition, base.endPosition, ref base.velocity, base.smoothTime);
 	}
 }

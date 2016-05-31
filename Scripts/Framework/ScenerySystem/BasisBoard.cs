5a9b3fb0714b4ad912da399397157122509de61b
Ernst <ernst_luring@hotmail.com>
2016-05-31 14:57:48 +0200

    Update BasisBoard.cs

--- Scripts/Framework/ScenerySystem/BasisBoard.cs
+++ Scripts/Framework/ScenerySystem/BasisBoard.cs
@@ -3,18 +3,11 @@ using System.Collections;
 
 public class BasisBoard : Board {
 
-	private float mSpeed = 6f;
-
 	void Update () {
 		Move();
 	}
 
 	public override void Move () {
-//		base.startPosition = myTransform.position;
-//		base.endPosition = base.startPosition + Vector3.back;
-
 		myTransform.Translate(Vector3.back * Time.deltaTime * base.speed, Space.World);
-
-//		transform.position = Vector3.SmoothDamp(base.startPosition, base.endPosition, ref base.velocity, base.smoothTime);
 	}
 }
